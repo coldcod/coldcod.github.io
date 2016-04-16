2bbd4324d26249d57d483142e7b53fc1c42142d8
Satwinderpal Singh <satwindersapra@ymail.com>
2016-04-16 21:25:24 +0530

    --

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '<validation_token>') {
+  if (req.query['hub.verify_token'] === 'coldcod_x_huemiser') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
